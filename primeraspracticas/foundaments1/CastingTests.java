42a913205cf75495b069a258861df80784474561
aurgonza <aurgonza@student.42urduliz.com>
2025-03-19 13:26:50 +0100

    feat: CastingTests sobre casting de datos
    He implementado la clase CastingTests y ajustado el main para visualizarlo.

--- primeraspracticas/foundaments1/CastingTests.java
+++ primeraspracticas/foundaments1/CastingTests.java
@@ -2,4 +2,58 @@ package foundaments1;
 
 public class CastingTests {
 
+	public void validCastExample() {
+		
+		char	c = '1'; //es 49 en ASCII, usando base decimal
+		int		i;
+		short	s;
+		long	l;
+		byte	b;
+		
+		System.out.println("Usando conversiones explícitas:");
+		System.out.println("c = " + c);
+		i = (int)c;
+		System.out.println("i = " + i);
+		s = (short)i;
+		System.out.println("s = " + s);
+		l = (long)s;
+		System.out.println("l = " + l);
+		b = (byte)l;
+		System.out.println("b = " + b);
+		System.out.println("Y usando conversiones implícitas:");
+		b = 1;
+		System.out.println("b = " + b);
+		s = b;
+		System.out.println("s = " + s);
+		i = s;
+		System.out.println("i = " + i);
+		l = i;
+		System.out.println("l = " + l);	
+		i = 49;
+		System.out.println("Nuevo valor de i = " + i);
+		c = (char)i;
+		System.out.println("c = (char)i de forma EXplícita -> c = " + c + ", no aparece error como tal");
+	}
+	public void invalidCastExample() {
+		int		i = 1;
+//		char	c;
+		boolean	bo = true;
+		String	infoCast1;
+		String	infoCast2;
+		
+		System.out.println("Conversiones inválidas (para ver los errores, DESCOMENTAR el código):");
+		System.out.println("(valor de i = " + i + ")");
+	//	c = i;
+		System.out.println("char c = i (aparece error si es char a int de forma IMplícita");
+	//	bo = i;
+	//	bo = (boolean)i;
+		System.out.println("\nUn tipo booleano no puede castearse con otro tipo ni implícita ni explícitamente");
+		System.out.println("bo = " + bo);
+		infoCast1 = "Para que sea válido un casteo entre datos numéricos debe respetarse que el valor \"quepa\"";
+		infoCast2 = " en el nuevo tipo, no se puede hacer por ejemplo byte = long; porque long es mas grande que byte.";
+		System.out.println(infoCast1 + infoCast2);
+	}
+	
+	
+	
 }
--- primeraspracticas/foundaments1/Main.java
+++ primeraspracticas/foundaments1/Main.java
@@ -4,9 +4,17 @@ public class Main {
 	public static void main(String []args) {
 		//por ahora no hace mucho
 		DiffRefToCopy obj1 = new DiffRefToCopy();
+		System.out.println("Ejemplos sobre copia de datos y de referencias");
 		obj1.copyExample();
 		System.out.println();
 		obj1.refExample();
+		obj1 = null;
+		System.out.println();
+		System.out.println("Ejemplos sobre casteos válidos e inválidos");
+		CastingTests obj2 = new CastingTests();
+		obj2.validCastExample();
+		System.out.println();
+		obj2.invalidCastExample();
 		System.out.println("\n......................................\nA la espera de nuevos métodos.");
 	}
 }
